3bae3f6d96126dec12b2480dc9e6ce1643d517a0
Jeremy Heyer <jheyer159@gmail.com>
2021-05-19 17:34:47 -0400

    feat: added auth and storage init

---
+++ packages/firebase/auth/init.ts
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+export const auth = firebase.auth();
+
+export function init(): firebase.auth.Auth {
+  const host = process.env.FIREBASE_AUTH_HOST || "localhost";
+  const port =
+    (process.env.FIREBASE_AUTH_PORT &&
+      parseInt(process.env.FIREBASE_AUTH_PORT)) ||
+    9099;
+  const isBrowser = typeof window !== "undefined";
+  if (
+    (isBrowser && location?.hostname === "localhost") ||
+    location?.hostname === "127.0.0.1" ||
+    process.env.FIREBASE_AUTH_EMULATOR === "true"
+  ) {
+    auth.useEmulator(`${host}:${port}`);
+  }
+  console.log("[Firebase] - Auth initialized: ", host + ":" + port);
+  return auth;
+}
+
+export default init;
--- packages/firebase/storage/init.ts
+++ packages/firebase/storage/init.ts
@@ -1,5 +1,25 @@
-import firebase from 'firebase/app';
-import 'firebase/storage';
+import firebase from "firebase/app";
+import "firebase/storage";
 
 export const storage = firebase.storage();
-console.log('[Firebase] - Storage initialized');
+console.log("[Firebase] - Storage initialized");
+
+export function init(): firebase.storage.Storage {
+  const host = process.env.FIREBASE_STORAGE_HOST || "localhost";
+  const port =
+    (process.env.FIREBASE_STORAGE_PORT &&
+      parseInt(process.env.FIREBASE_STORAGE_PORT)) ||
+    9199;
+  const isBrowser = typeof window !== "undefined";
+  if (
+    (isBrowser && location?.hostname === "localhost") ||
+    location.hostname === "127.0.0.1" ||
+    process.env.FIREBASE_STORAGE_EMULATOR === "true"
+  ) {
+    console.log("[Firebase] - Storage initialized: ", host + ":" + port);
+    storage.useEmulator(host, port);
+  }
+  return storage;
+}
+
+export default init;
