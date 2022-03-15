be8930a4a07260077c0d0f81a16c880fdf5b7094
Jeremy Heyer <jheyer159@gmail.com>
2022-03-15 14:50:40 -0500

    perf: move getAuth to effect

--- packages/firebase-auth-next/use-user.tsx
+++ packages/firebase-auth-next/use-user.tsx
@@ -1,9 +1,12 @@
 import { FirebaseApp } from "firebase/app";
-import { getAuth, onIdTokenChanged, signOut, User } from "firebase/auth";
+import { Auth, getAuth, onIdTokenChanged, signOut, User } from "firebase/auth";
+import { Firestore } from "firebase/firestore";
+import { Functions } from "firebase/functions";
+import { FirebaseStorage } from "firebase/storage";
 import React, {
   createContext,
-  ReactNode,
-  useCallback,
+  FC,
+  Context as TContext,
   useContext,
   useEffect,
   useState,
@@ -15,58 +18,76 @@ type Props = {
   readonly logout: () => Promise<void>;
 };
 
+type Context = {
+  context: TContext<{
+    app: FirebaseApp;
+    firestore: Firestore;
+    functions: Functions;
+    storage: FirebaseStorage;
+  }>;
+};
+
 export const Context = createContext<Props>({
   initializing: true,
   user: undefined,
   logout: () => Promise.reject("no user auth provider"),
 });
 
-export const Provider = ({
-  app,
+export const Provider: FC<Context> = ({
+  context,
   children,
-}: {
-  app: FirebaseApp;
-  readonly children: ReactNode;
 }): JSX.Element | null => {
+  const { app } = useContext(context);
   const [initializing, setInitializing] = useState(true);
   const [user, setUser] = useState<User>();
+  const [auth, setAuth] = useState<Auth>();
 
-  const auth = getAuth(app);
-
-  const logout = useCallback(async () => {
-    await signOut(auth);
-    setUser(undefined);
-  }, []);
+  const logout = async () => {
+    if (auth) {
+      await signOut(auth);
+      setUser(undefined);
+    }
+  };
 
   useEffect(() => {
     // Firebase updates the id token every hour, this
     // makes sure the react state and the cookie are
     // both kept up to date
-    const cancelAuthListener = onIdTokenChanged(
-      auth,
-      (user) => {
-        if (user) {
-          setUser(user);
-        } else {
-          setUser(undefined);
-        }
-        setInitializing(false);
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-    return () => {
-      cancelAuthListener();
-    };
-  }, []);
+    if (app) {
+      setAuth(getAuth(app));
+    }
+    return;
+  }, [app]);
 
-  const context = {
+  useEffect(() => {
+    if (auth) {
+      const cancelAuthListener = onIdTokenChanged(
+        auth,
+        (user) => {
+          if (user) {
+            setUser(user);
+          } else {
+            setUser(undefined);
+          }
+          setInitializing(false);
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+      return () => {
+        cancelAuthListener();
+      };
+    }
+    return;
+  }, [auth]);
+
+  const value = {
     initializing,
     logout,
     user,
   };
-  return <Context.Provider value={context}>{children}</Context.Provider>;
+  return <Context.Provider {...{ value }}>{children}</Context.Provider>;
 };
 
 export const useUser = (): Props => {
